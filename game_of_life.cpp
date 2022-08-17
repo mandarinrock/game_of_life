5ef967276e00a06a8e126ef55979adf2976ffaec
mandarinrock <mandarinrock.dg@gmail.com>
2022-08-17 12:22:50 -0700

    Added randomBoard()

--- game_of_life.cpp
+++ game_of_life.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <vector>
+#include <time.h>
 
 using namespace std;
 
@@ -23,10 +24,22 @@ void printBoard(vector<vector<bool>> board) {
 
 }
 
+vector<vector<bool>> randomBoard(vector<vector<bool>> board) {
+    srand(time(NULL));
+    for(int i = 0; i < board.size(); i++) {
+        for(int j = 0; j < board[0].size(); j++) {
+            board[i][j] = rand() % 2;
+        }
+    }
+    return board;
+}
+
 
 int main() {
 
     vector<vector<bool>> board(5, vector<bool>(5, false));
+    printBoard(board);
+    board = randomBoard(board);
     printBoard(board);
 
     cout << "Successful run, terminating" << endl;
