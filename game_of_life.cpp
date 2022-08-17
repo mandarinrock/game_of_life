9e122ac710a154a746e93a2a6d191e4a192e6bd8
mandarinrock <mandarinrock.dg@gmail.com>
2022-08-17 12:18:30 -0700

    Added printBoard()

--- game_of_life.cpp
+++ game_of_life.cpp
@@ -1,8 +1,34 @@
 #include <iostream>
+#include <vector>
 
 using namespace std;
 
+/**
+ * @brief 
+ * 
+ * @param board 
+ */
+void printBoard(vector<vector<bool>> board) {
+
+    for(int i = 0; i < board.size(); i++) {
+        for(int j = 0; j < board[0].size(); j++) {
+            if(board[i][j]) {
+                cout << "*";
+            } else {
+                cout << " ";
+            }
+        }
+        cout << endl;
+    }
+
+}
+
+
 int main() {
-    cout << "Hello, World!" << endl;
+
+    vector<vector<bool>> board(5, vector<bool>(5, false));
+    printBoard(board);
+
+    cout << "Successful run, terminating" << endl;
     return 0;
 }
